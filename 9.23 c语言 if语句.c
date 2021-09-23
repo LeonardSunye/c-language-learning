82899d230e60036e1f3a5b4879f3b4b22ffcbb93
randysunye <90534648+randysunye@users.noreply.github.com>
2021-09-23 10:36:22 +0800

    9.23 c

---
+++ 9.23 c语言 if语句.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main ()
+{
+	int day=0;
+	int n=1;
+	scanf("%d",&day);
+	switch(day)
+	{
+		case 1:
+			printf("the day of work\n");
+				break;
+		case 2:
+			printf("the day of rest\n");
+			break;
+	} 
+	
+	return 0;
+ } 
